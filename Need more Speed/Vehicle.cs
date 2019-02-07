9a96d79fb501778ccc0339eb06f67caf3dbd14ba
Timo Jaeger <timo.jaeger@endress.com>
2019-02-07 11:27:03 +0100

    update Vehicle

--- Need more Speed/Vehicle.cs
+++ Need more Speed/Vehicle.cs
@@ -14,6 +14,10 @@ namespace Need_more_Speed
         private double rotation;
         private double speed;
         private char type;
+        private bool up;
+        private bool down;
+        private bool left;
+        private bool right;
 
 
         public byte Compare_to_player { get => compare_to_player; set => compare_to_player = value; }
@@ -22,22 +26,34 @@ namespace Need_more_Speed
         public double Rotation { get => rotation; set => rotation = value; }
         public double Speed { get => speed; set => speed = value; }
         public char Type { get => type; set => type = value; }
+        public bool Up { get => up; set => up = value; }
+        public bool Down { get => down; set => down = value; }
+        public bool Left { get => left; set => left = value; }
+        public bool Right { get => right; set => right = value; }
 
         public Vehicle(char type,byte compare_to_player)
         {
 
         }
 
-        public void update_speed()
+
+        private void update_speed()
         {
 
 
         }
+
 
         private void refresh_position()
         {
 
         }
+
+        private void position_calculation(double positon_y, double positon_x)
+        {
+
+        }
+
 
     }
 }
