70e82da4fe67e95f500b92eccdf66622b48feb33
Timo Jaeger <timo.jaeger@endress.com>
2019-02-07 10:50:07 +0100

    Vehocle ner erstellt

---
+++ Need more Speed/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Need_more_Speed
+{
+    class Vehicle
+    {
+        private byte compare_to_player;
+        private double positon_x;
+        private double positon_y;
+        private double rotation;
+        private double speed;
+        private char type;
+
+
+        public byte Compare_to_player { get => compare_to_player; set => compare_to_player = value; }
+        public double Positon_x { get => positon_x; set => positon_x = value; }
+        public double Positon_y { get => positon_y; set => positon_y = value; }
+        public double Rotation { get => rotation; set => rotation = value; }
+        public double Speed { get => speed; set => speed = value; }
+        public char Type { get => type; set => type = value; }
+
+
+
+
+    }
+}
+
