9e918bcc3fc8d570a0e31eae0a2421e8331b8963
Timo Jaeger <timo.jaeger@endress.com>
2019-02-07 11:53:50 +0100

    big update Vehicle

--- Need more Speed/Vehicle.cs
+++ Need more Speed/Vehicle.cs
@@ -21,8 +21,8 @@ namespace Need_more_Speed
 
 
         public byte Compare_to_player { get => compare_to_player; set => compare_to_player = value; }
-        public double Positon_x { get => positon_x; set => positon_x = value; }
-        public double Positon_y { get => positon_y; set => positon_y = value; }
+        public double Position_x { get => positon_x; set => positon_x = value; }
+        public double Position_y { get => positon_y; set => positon_y = value; }
         public double Rotation { get => rotation; set => rotation = value; }
         public double Speed { get => speed; set => speed = value; }
         public char Type { get => type; set => type = value; }
@@ -39,19 +39,72 @@ namespace Need_more_Speed
 
         private void update_speed()
         {
-
+            int zaehler = 0;
+            if ((up == true) && (speed > -20))
+            {
+                speed--;
+                if (speed > 1)
+                {
+                    speed--;
+                    speed--;
+                }
+            }
+            else if ((down == true) && (speed < 40))
+            {
+                speed++;
+                if (speed < -1)
+                {
+                    speed++;
+                    speed++;
+                }
+            }
+            else if (zaehler >= 2)
+            {
+                if (speed > 0) /*&& (speed < 25)*/
+                {
+                    speed--;
+                    // Auto_red_geschwindigkeit--;
+                }
+                else if (speed < 0)/* && (speed < -15)*/
+                {
+                    speed++;
+                    
+                }
+                zaehler = 0;
+            }
+            else
+            {
+                zaehler++;
+            }
 
         }
 
 
         private void refresh_position()
         {
-
+            Canvas.SetLeft(Auto_red, position_x);
+            Canvas.SetTop(Auto_red, position_y);
         }
 
-        private void position_calculation(double positon_y, double positon_x)
+        private void position_calculation(double position_y, double position_x)
         {
+            if (speed != 0)
+            {
+                if (left == true)
+                {
+                    rotation++;
+                    rotation++;
+                }
 
+                if (right == true)
+                {
+                    rotation--;
+                    rotation--;
+                }
+            }
+            double angle = Math.PI * rotation / 180.0;
+            position_x += (speed / 10) * Math.Cos(angle);
+            position_y -= (speed / 10) * Math.Sin(angle);
         }
 
 
