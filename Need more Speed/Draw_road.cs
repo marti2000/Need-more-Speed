fc72ec77c197141f0fa2eb4ccd976f8b85ba0a11
Timo Jaeger <timo.jaeger@endress.com>
2019-02-28 11:43:52 +0100

    Curve funktion erstellt

--- Need more Speed/Draw_road.cs
+++ Need more Speed/Draw_road.cs
@@ -45,29 +45,81 @@ namespace Need_more_Speed
                 Line[] street = new Line[Convert.ToInt16(grid) + 1];
                 street[Convert.ToInt16(x_curve)] = new Line() { Name = "street" + Convert.ToInt16(x_curve) };
                 street[Convert.ToInt16(x_curve)].Stroke = Brushes.Gray;
-                street[Convert.ToInt16(x_curve)].X1 = 1;
-                street[Convert.ToInt16(x_curve)].X2 = 50;
-                street[Convert.ToInt16(x_curve)].Y1 = 1;
-                street[Convert.ToInt16(x_curve)].Y2 = 50;
+                street[Convert.ToInt16(x_curve)].X1 = x_offset;
+                street[Convert.ToInt16(x_curve)].X2 = x_offset + Convert.ToInt16(x_curve);
+                street[Convert.ToInt16(x_curve)].Y1 = y_offset;
+                street[Convert.ToInt16(x_curve)].Y2 = y_offset - Convert.ToInt16(y_curve); ;
 
                 street[Convert.ToInt16(x_curve)].StrokeThickness = 2;
                 myCanvas.Children.Add(street[Convert.ToInt16(x_curve)]);
 
             }
-
-
         }
-        public void curve_90Degree(int x, int y)
+        public void curve_90Degree(int x_offset, int y_offset, double grid)
         {
+            double x_curve = 0;
+            double y_curve = 0;
 
+            for (x_curve = 0; x_curve <= grid; x_curve++)
+            {
+                y_curve = Math.Sqrt(Math.Pow(grid, 2) - Math.Pow(x_curve, 2));
+
+                Line[] street = new Line[Convert.ToInt16(grid) + 1];
+                street[Convert.ToInt16(x_curve)] = new Line() { Name = "street" + Convert.ToInt16(x_curve) };
+                street[Convert.ToInt16(x_curve)].Stroke = Brushes.Gray;
+                street[Convert.ToInt16(x_curve)].X1 = x_offset;
+                street[Convert.ToInt16(x_curve)].X2 = x_offset + Convert.ToInt16(x_curve);
+                street[Convert.ToInt16(x_curve)].Y1 = y_offset;
+                street[Convert.ToInt16(x_curve)].Y2 = y_offset + Convert.ToInt16(y_curve); ;
+
+                street[Convert.ToInt16(x_curve)].StrokeThickness = 2;
+                myCanvas.Children.Add(street[Convert.ToInt16(x_curve)]);
+
+            }
         }
-        public void curve_180Degree(int x, int y)
+        public void curve_180Degree(int x_offset, int y_offset, double grid)
         {
+            double x_curve = 0;
+            double y_curve = 0;
 
+            for (x_curve = 0; x_curve <= grid; x_curve++)
+            {
+                y_curve = Math.Sqrt(Math.Pow(grid, 2) - Math.Pow(x_curve, 2));
+
+                Line[] street = new Line[Convert.ToInt16(grid) + 1];
+                street[Convert.ToInt16(x_curve)] = new Line() { Name = "street" + Convert.ToInt16(x_curve) };
+                street[Convert.ToInt16(x_curve)].Stroke = Brushes.Gray;
+                street[Convert.ToInt16(x_curve)].X1 = x_offset;
+                street[Convert.ToInt16(x_curve)].X2 = x_offset +grid - Convert.ToInt16(x_curve);
+                street[Convert.ToInt16(x_curve)].Y1 = y_offset;
+                street[Convert.ToInt16(x_curve)].Y2 = y_offset - Convert.ToInt16(y_curve); ;
+
+                street[Convert.ToInt16(x_curve)].StrokeThickness = 2;
+                myCanvas.Children.Add(street[Convert.ToInt16(x_curve)]);
+
+            }
         }
-        public void curve_270Degree(int x, int y)
+        public void curve_270Degree(int x_offset, int y_offset, double grid)
         {
+            double x_curve = 0;
+            double y_curve = 0;
 
+            for (x_curve = 0; x_curve <= grid; x_curve++)
+            {
+                y_curve = Math.Sqrt(Math.Pow(grid, 2) - Math.Pow(x_curve, 2));
+
+                Line[] street = new Line[Convert.ToInt16(grid) + 1];
+                street[Convert.ToInt16(x_curve)] = new Line() { Name = "street" + Convert.ToInt16(x_curve) };
+                street[Convert.ToInt16(x_curve)].Stroke = Brushes.Gray;
+                street[Convert.ToInt16(x_curve)].X1 = x_offset;
+                street[Convert.ToInt16(x_curve)].X2 = x_offset + grid - Convert.ToInt16(x_curve);
+                street[Convert.ToInt16(x_curve)].Y1 = y_offset;
+                street[Convert.ToInt16(x_curve)].Y2 = y_offset + Convert.ToInt16(y_curve); ;
+
+                street[Convert.ToInt16(x_curve)].StrokeThickness = 2;
+                myCanvas.Children.Add(street[Convert.ToInt16(x_curve)]);
+
+            }
         }
     }
 }
