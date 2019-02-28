ad595989cef232216f1d1e046193f27995154130
Timo Jaeger <timo.jaeger@endress.com>
2019-02-28 11:08:52 +0100

    curve_0Degree überarbeitet

--- Need more Speed/Draw_road.cs
+++ Need more Speed/Draw_road.cs
@@ -8,6 +8,7 @@ namespace Need_more_Speed
 {
     class Draw_road
     {
+
         public void straight_horizontal(int x, int y)
         {
             
@@ -16,8 +17,18 @@ namespace Need_more_Speed
         {
 
         }
-        public void curve_0Degree(int x, int y)
+        public void curve_0Degree(int x_offset, int y_offset, double grid)
         {
+            double x_curve = 0;
+            double y_curve = 0;
+
+            for (x_curve = 0; x_curve <= grid; x_curve++) 
+            {
+                y_curve = Math.Sqrt(Math.Pow(grid, 2) - Math.Pow(x_curve, 2));
+
+
+            }
+
 
         }
         public void curve_90Degree(int x, int y)
