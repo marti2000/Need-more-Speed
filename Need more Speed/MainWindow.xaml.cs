4c98452eca37c87e778d1db4e0434118b51c22c1
Marcel Vögeli <marcel.voegeli@breitband.ch>
2019-03-27 19:00:25 +0100

    include Menü

--- Need more Speed/MainWindow.xaml.cs
+++ Need more Speed/MainWindow.xaml.cs
@@ -23,11 +23,13 @@ namespace Need_more_Speed
     public partial class MainWindow : Window
     {
 
-        public const double Grid = 100;
+        public const double Grid = 100; //To define the size of the Raceingtrack
 
+        //include the Cars global
         Vehicle red_car;
         Vehicle blue_car;
 
+        //include the Map global
         Maps Map;
 
 
@@ -35,122 +37,57 @@ namespace Need_more_Speed
         {
             InitializeComponent();
 
+            //generate a Menu
+            Menue menue = new Menue();
+            menue.Show();
+            //while(menue.Start_the_game == false);
+
+            //Generate the timer for the on the road check
+            /*
+             *
+             * TODD move into a other class
+             *
+             */
             System.Windows.Threading.DispatcherTimer refresh = new System.Windows.Threading.DispatcherTimer();
             refresh.Tick += refresh_Tick;
             refresh.Interval = new TimeSpan(0, 0, 0, 0, 1);
             refresh.Start();
 
-            Dictionary<string, Draw_road> road_planner = new Dictionary<string, Draw_road>();
-
-            Draw_road Road = new Draw_road(racingtrack);
-
+            //generate the Map
             Map = new Maps(racingtrack);
             Map.chose_Map(0, Grid);
 
-            MediaElement background_sound = new MediaElement();
 
-            Backgroundsound.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
-            Backgroundsound.Play();
-            
-            //Backgroundsound.
-
-            //Path source_background_sound = new Path();
-
-
-            /*string startPath = @"C:\Benutzer\";//Der absolute Ausgangspfad
-            string relative = @"..\Windows\explorer.exe";//Der relative Pfad
-            string st4ring = @"..\Windows\explorer.exe";//Der relative Pfad
-            string absolut = Path.GetFullPath(Path.Combine(startPath, relative));//Die ermittlung eines absoluten Pfades*/
-
-            background_sound.Source = new Uri("Need-more-Speed/Julien Bam - Ready to fight(prod. by Vincent Lee).mp3", UriKind.Relative); //@"C:\Users\Marcel Vögeli\source\repos\marti2000\Need-more-Speed";//"..//Need-more-Speed//Julien Bam - Ready to fight(prod. by Vincent Lee).mp3";
-
-            background_sound.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
-
-            background_sound.Volume = 100;
-
-            background_sound.Play();
-
-            Uri source = new Uri(@"Need-more-Speed/Julien Bam - Ready to fight(prod. by Vincent Lee).mp3", UriKind.Relative);
-
-
-
-            MediaElement test = new MediaElement();
-            test.Source = new Uri(@"C:\Users\Marcel Vögeli\source\repos\marti2000\Need-more-Speed\Julien Bam - Ready to fight(prod. by Vincent Lee).mp3");//@"C:\Users\Marcel Vögeli\source\repos\marti2000\Need-more-Speed\Julien Bam - Ready to fight(prod. by Vincent Lee).mp3";
-            //(new Uri("Need-more-Speed/Julien Bam - Ready to fight(prod. by Vincent Lee).mp3", UriKind.RelativeOrAbsolute)/*new Uri(source.AbsoluteUri)*/);
-            //Path.GetFullPath((new Uri(absolute_path)).LocalPath);
-            //test.Load();
-            test.LoadedBehavior = System.Windows.Controls.MediaState.Manual;
-            test.Play();
-            /*
-            Road.curve_0Degree(1, 1, 100);
-            Road.curve_90Degree(2, 1, 100);
-            Road.curve_180Degree(2, 2, 100);
-            Road.curve_270Degree(1, 2, 100);
-
-            Road.straight_horizontal(3, 3, 100);
-            */
-            road_planner.Add("straight.horizontal", Road); 
-            road_planner.Add("straight.vertical", Road);
-            road_planner.Add("curve.0Degree", Road);
-            road_planner.Add("curve.90Degree", Road);
-            road_planner.Add("curve.180Degree", Road);
-            road_planner.Add("curve.270Degree", Road);
-
+            //Creating the different Cars for the Race
             red_car = new Vehicle("Car", 1, 145, 365, racingtrack, new TextBlock()/*Speed*/, Brushes.Red);
             red_car.Rotation = 270;
 
             blue_car = new Vehicle("Car", 2, 165, 365, racingtrack, new TextBlock(), Brushes.Blue);
             blue_car.Rotation = 270;
-            /*red_car.Position_x = 200;
-            red_car.Position_y = 200;
-            red_car.Left = true;
-            red_car.Down = true;*/
-            
-                
-                Line line= new Line();
-                line.Stroke = Brushes.Gray;
-                line.StrokeThickness = 10;
-                line.X1 = 1250;
-                line.X2 = 1250;
-                line.Y1 = 0;
-                line.Y2 = 760;
-                racingtrack.Children.Add(line);
 
 
-            //int tester = road_planner["straight.horizontal"];
-            /*
-            for(int i = 0; i>10; i++)
-            {
-                Line[] street = new Line[10];
-                street[i] = new Line() { Name = "street" + i };
-                street[i].Stroke = Brushes.Gray;
-                street[i].X1 = 1;
-                street[i].X2 = 50;
-                street[i].Y1 = 1; 
-                street[i].Y2 = 50;
-
-                street[i].StrokeThickness = 2;
-                racingtrack.Children.Add(street[i]);
-            }*/
+            //Testline for develop the rebounce
+            Line line = new Line();
+            line.Stroke = Brushes.Gray;
+            line.StrokeThickness = 10;
+            line.X1 = 1250;
+            line.X2 = 1250;
+            line.Y1 = 0;
+            line.Y2 = 760;
+            racingtrack.Children.Add(line);
 
 
-            /*SolidColorBrush street = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
-            Pen pen = new Pen(street, 10);
-            Background. .DrawLine(pen, 20, 10, 300, 100);*/
-
-
-
-            /*street.X1 = 10;
-            street.X2 = 50;
-            street.Y1 = 10;
-            street.Y2 = 50;
-
-            street.StrokeThickness = 2;
-            racingtrack.Children.Add(street);*/
         }
+
+        /*
+         * 
+         * TODO Move the Key events (KeyDown and KeyUp) in a other Class
+         * 
+         */
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            //Keys for Player 1
             if (e.Key == Key.W)
             {
                 red_car.Up = true;
@@ -168,6 +105,7 @@ namespace Need_more_Speed
                 red_car.Right = true;
             }
 
+            //Keys for Player 2
             if (e.Key == Key.Up)
             {
                 blue_car.Up = true;
@@ -188,6 +126,7 @@ namespace Need_more_Speed
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
+            //Keys for Player 1
             if (e.Key == Key.W)
             {
                 red_car.Up = false;
@@ -205,6 +144,7 @@ namespace Need_more_Speed
                 red_car.Right = false;
             }
 
+            //Keys for Player 2
             if (e.Key == Key.Up)
             {
                 blue_car.Up = false;
@@ -225,12 +165,14 @@ namespace Need_more_Speed
 
         private void Backgroundsound_MediaEnded(object sender, RoutedEventArgs e)
         {
+            //endless loop of backgroundmusic
             Backgroundsound.Position = new TimeSpan(0);
             Backgroundsound.Play();
         }
 
         private void refresh_Tick(object sender, EventArgs e)
         {           
+            //Check if the car is on the Road
             if(Map.car_on_road(red_car.Position_x, red_car.Position_y, Grid, red_car.Rotation) == true)
             {
                 Speed.Text = "ON Road";
---
+++ Need more Speed/Menue.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Need_more_Speed
+{
+    /// <summary>
+    /// Interaktionslogik für Menue.xaml
+    /// </summary>
+    public partial class Menue : Window
+    {
+        private bool start_the_game = false;
+
+        public bool Start_the_game { get => start_the_game; set => start_the_game = value; }
+
+        public Menue()
+        {
+            InitializeComponent();
+            Menue_screen.Width = 800;
+            next_left.Content = "<";
+            next_right.Content = ">";
+
+            next_right_player_1.Content = ">";
+            next_left_player_1.Content = "<";
+            next_right_player_2.Content = ">";
+            next_left_player_2.Content = "<";
+        }
+
+        private void change_track_visible()
+        {
+            next_left.Visibility = Visibility.Visible;
+            next_right.Visibility = Visibility.Visible;
+            racetrack_show.Visibility = Visibility.Visible;
+            map_generator.Visibility = Visibility.Visible;
+
+            next_left.IsEnabled = true;
+            next_right.IsEnabled = true;
+            map_generator.IsEnabled = true;
+        }
+
+        private void change_track_hidden()
+        {
+            next_left.Visibility = Visibility.Hidden;
+            next_right.Visibility = Visibility.Hidden;
+            racetrack_show.Visibility = Visibility.Hidden;
+            map_generator.Visibility = Visibility.Hidden;
+
+            next_left.IsEnabled = false;
+            next_right.IsEnabled = false;
+            map_generator.IsEnabled = false;
+        }
+
+        private void change_car_visible()
+        {
+            next_left_player_1.Visibility = Visibility.Visible;
+            next_right_player_1.Visibility = Visibility.Visible;
+            next_left_player_2.Visibility = Visibility.Visible;
+            next_right_player_2.Visibility = Visibility.Visible;
+            show_car_player_1.Visibility = Visibility.Visible;
+            show_car_player_2.Visibility = Visibility.Visible;
+
+            next_left_player_1.IsEnabled = true;
+            next_right_player_1.IsEnabled = true;
+            next_left_player_2.IsEnabled = true;
+            next_right_player_2.IsEnabled = true;
+        }
+
+        private void change_car_hidden()
+        {
+            next_left_player_1.Visibility = Visibility.Hidden;
+            next_right_player_1.Visibility = Visibility.Hidden;
+            next_left_player_2.Visibility = Visibility.Hidden;
+            next_right_player_2.Visibility = Visibility.Hidden;
+            show_car_player_1.Visibility = Visibility.Hidden;
+            show_car_player_2.Visibility = Visibility.Hidden;
+
+            next_left_player_1.IsEnabled = false;
+            next_right_player_1.IsEnabled = false;
+            next_left_player_2.IsEnabled = false;
+            next_right_player_2.IsEnabled = false;
+        }
+
+        private void Change_track_Click(object sender, RoutedEventArgs e)
+        {
+            change_track_visible();
+            change_car_hidden();
+        }
+
+        private void Change_car_Click(object sender, RoutedEventArgs e)
+        {
+            change_car_visible();
+            change_track_hidden();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            change_car_hidden();
+            change_track_hidden();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            start_the_game = true;
+        }
+
+        private void Next_left_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_right_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Map_generator_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_left_player_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_right_player_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_right_player_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_left_player_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
